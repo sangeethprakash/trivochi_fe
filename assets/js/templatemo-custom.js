20b9d7ebdc18ad2f383e67a2b6026a8acd6d5238
sangeeth prakash <sangeeth.sangeeth38@gmail.com>
2024-12-17 22:50:31 +0530

    phone number added in form

--- assets/js/templatemo-custom.js
+++ assets/js/templatemo-custom.js
@@ -140,6 +140,7 @@
   window.clearForm = function () {
     document.getElementById("name").value = "";
     document.getElementById("email").value = "";
+    document.getElementById("phone").value = "";
     document.getElementById("message").value = "";
   };
 })(window.jQuery);
